16090ffbd74c142fbd9c2484d62826363c58b807
tsukigahara <satsuki.tksm@gmail.com>
2023-02-01 11:27:55 +0900

    connected to mysql database

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $movies = Movie::all();
+
+        var_dump($movies);
+
+        return view('welcome');
+    }
 }
